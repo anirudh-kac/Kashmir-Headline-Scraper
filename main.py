af2f1784c24b9aa84d79b8b59a6bde139da9bc64
Anirudh Kachroo <anirudh.kac@yahoo.com>
2021-08-01 23:24:42 +0530

    add gk scraper , gitignore

--- main.py
+++ main.py
@@ -1,1 +1,3 @@
-print("Hello")
+from scrapers.greater_kashmir import scraper as gk_scraper
+
+gk_scraper()
--- scrapers/greater_kashmir.py
+++ scrapers/greater_kashmir.py
@@ -1,1 +1,24 @@
+from selenium import webdriver
+from bs4 import BeautifulSoup
+
+BASE_URL = "https://www.greaterkashmir.com/collection/top-stories"
+
+driver = webdriver.Firefox()
+
+def scraper():
+    driver.get(BASE_URL)
+    soup = BeautifulSoup(driver.page_source,"html5lib")
+
+    headlines = soup.find_all('h6')
+
+    print(headlines)
+
+    for headline in headlines:
+        print(headline.get_text())
+        print()
+        print("-------------------------------------------------------------------------")
+
+    ##print(soup.prettify())
+
+
 
