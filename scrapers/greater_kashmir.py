fa803e09a0f15f336bbe9e1bd26be88f2985dffd
Anirudh Kachroo <anirudh.kac@yahoo.com>
2021-08-01 23:34:47 +0530

    write data to archives

--- scrapers/greater_kashmir.py
+++ scrapers/greater_kashmir.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 from bs4 import BeautifulSoup
-
+from datetime import date
+from csv import writer
 BASE_URL = "https://www.greaterkashmir.com/collection/top-stories"
 
 driver = webdriver.Firefox()
@@ -12,12 +13,17 @@ def scraper():
     headlines = soup.find_all('h6')
 
     print(headlines)
+    filename = str(date.today())
 
+    file = open("./archives/greater_kashmir/" + filename + ".csv",'w')
+    csv_writer = writer(file)
     for headline in headlines:
+        csv_writer.writerow([headline.get_text()])
         print(headline.get_text())
         print()
         print("-------------------------------------------------------------------------")
 
+    file.close()
     ##print(soup.prettify())
 
 
