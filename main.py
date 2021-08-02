426323246c9ef57f23225b3ad2bfc80cf4ede2d2
Anirudh Kachroo <anirudh.kac@yahoo.com>
2021-08-02 16:16:24 +0530

    handle command line arguments

--- main.py
+++ main.py
@@ -1,8 +1,30 @@
+import sys
+
 from scrapers.greater_kashmir import scraper as gk_scraper
 from scrapers.rising_kashmir import scraper as rk_scraper
 from scrapers.kashmir_observer import scraper as ko_scraper
 
-##gk_scraper()
-##rk_scraper()
+def main():
 
-ko_scraper()
+    if len(sys.argv)!=2 :
+        print("Usage : python main.py gk | rk | ko")
+        print("gk - Greater Kashmir")
+        print("rk - Rising  Kashmir")
+        print("ko - Kashmir Observer")
+        return
+    else:
+
+        keyword = sys.argv[1]
+        if keyword == "gk":
+            gk_scraper()
+        elif keyword =="rk":
+            rk_scraper()
+        elif keyword == "ko":
+            ko_scraper()
+        else:
+            print("Invalid keyword")
+
+
+
+if __name__ == "__main__":
+    main()
