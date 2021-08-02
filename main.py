97584479067dd4279e4518e67117f4c948643f8e
Anirudh Kachroo <anirudh.kac@yahoo.com>
2021-08-02 16:26:27 +0530

    add help

--- main.py
+++ main.py
@@ -4,10 +4,14 @@ from scrapers.greater_kashmir import scraper as gk_scraper
 from scrapers.rising_kashmir import scraper as rk_scraper
 from scrapers.kashmir_observer import scraper as ko_scraper
 
+from utils.help import print_help
+
 def main():
 
     if len(sys.argv)!=2 :
-        print("Usage : python main.py gk | rk | ko")
+        print("Usage : python main.py gk | rk | ko | help")
+
+        print("help - Get Help")
         print("gk - Greater Kashmir")
         print("rk - Rising  Kashmir")
         print("ko - Kashmir Observer")
@@ -15,14 +19,16 @@ def main():
     else:
 
         keyword = sys.argv[1]
-        if keyword == "gk":
+        if keyword == "help":
+            print_help()
+        elif keyword == "gk":
             gk_scraper()
         elif keyword =="rk":
             rk_scraper()
         elif keyword == "ko":
             ko_scraper()
         else:
-            print("Invalid keyword")
+            print("Invalid keyword\n Run 'main.py help' to get help")
 
 
 
---
+++ utils/help.py
@@ -0,0 +1,17 @@
+def print_help():
+    help = """
+
+        This python script scrapes top headlines from major newspapers of kashmir\n
+        The headlines will be printed to console and also written to respective csv files in archives folder\n
+        Usage : python main.py [keyword] \n
+        
+        keywords : \n
+        help : Prints help \n
+        gk : Scrapes and prints headlines from Greater Kashmir\n
+        rk : Scrapes and prints headlines from Rising Kashmir\n
+        ko : Scrapes and prints headlines from Kashmir Observer\n
+
+
+    """
+
+    print(help)
